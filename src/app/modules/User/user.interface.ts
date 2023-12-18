cf2c93e2c229e7de52a4a7cc9ce3dd302034e758
MDMinhazulIslamKhan <minhazulislamkhan@gmail.com>
2023-12-19 02:15:29 +0600

    #task-18 user=>model and interface

--- src/app/modules/User/user.interface.ts
+++ src/app/modules/User/user.interface.ts
@@ -1,25 +1,33 @@
-import { Model, Types } from "mongoose";
+import { Model, Types } from 'mongoose'
+import { Group } from '../Donor/donation.constant'
 
 export type IUser = {
-  name: string;
-  email: string;
-  password: string;
-};
+  name: string
+  email: string
+  password: string
+  phoneNumber: string
+  bloodGroup: Group
+  totalDonation: number
+  lastDonation?: Date
+  totalReceived: number
+  available: boolean
+  notification: number
+}
 
 export type IUserExist = {
-  password: string;
-  email: string;
-  name: string;
-  _id: Types.ObjectId | undefined;
-};
+  password: string
+  email: string
+  name: string
+  _id: Types.ObjectId | undefined
+}
 
 export type UserModel = {
   isUserExist(
     email: string
-  ): Promise<Pick<IUserExist, "_id" | "email" | "name" | "password">>;
+  ): Promise<Pick<IUserExist, '_id' | 'email' | 'name' | 'password'>>
 
   isPasswordMatched(
     givenPassword: string,
     savedPassword: string
-  ): Promise<boolean>;
-} & Model<IUser>;
+  ): Promise<boolean>
+} & Model<IUser>
--- src/app/modules/User/user.modal.ts
+++ src/app/modules/User/user.modal.ts
@@ -1,55 +1,85 @@
-import { Schema, model } from "mongoose";
-import { IUser, IUserExist, UserModel } from "./user.interface";
-import bcrypt from "bcrypt";
-import config from "../../../config";
+import { Schema, model } from 'mongoose'
+import { IUser, IUserExist, UserModel } from './user.interface'
+import bcrypt from 'bcrypt'
+import config from '../../../config'
+import { BloodGroups } from '../Donor/donation.constant'
 
 export const userSchema = new Schema<IUser, UserModel>({
   name: {
-    type: "string",
+    type: String,
     required: true,
   },
+  phoneNumber: {
+    type: String,
+    required: true,
+    unique: true,
+  },
   email: {
-    type: "string",
+    type: String,
     required: true,
   },
   password: {
-    type: "string",
+    type: String,
     required: true,
     select: 0,
   },
-});
+  bloodGroup: {
+    type: String,
+    enum: BloodGroups,
+    required: true,
+  },
+  totalDonation: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  lastDonation: {
+    type: Date,
+  },
+  totalReceived: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+  notification: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+})
 
 userSchema.statics.isUserExist = async function (
   email: string
 ): Promise<
-  Partial<Pick<IUserExist, "_id" | "password" | "name" | "email"> | null>
+  Partial<Pick<IUserExist, '_id' | 'password' | 'name' | 'email'> | null>
 > {
-  const user = await User.findOne(
-    { email },
-    { email: 1, name: 1, password: 1 }
-  );
+  const user = await User.findOne({ email }, { email: 1, name: 1, password: 1 })
 
-  console.log("from login", user);
-  return user;
-};
+  console.log('from login', user)
+  return user
+}
 
 userSchema.statics.isPasswordMatched = async function (
   givenPassword: string,
   savedPassword: string
 ): Promise<boolean> {
-  const isMatched = await bcrypt.compare(givenPassword, savedPassword);
-  console.log("pasword.....", isMatched);
+  const isMatched = await bcrypt.compare(givenPassword, savedPassword)
+  console.log('pasword.....', isMatched)
 
-  return isMatched;
-};
+  return isMatched
+}
 
-userSchema.pre("save", async function (next) {
+userSchema.pre('save', async function (next) {
   ///hasing User Password
   this.password = await bcrypt.hash(
     this.password,
     Number(config.bcrypt_salt_rounds)
-  );
-  next();
-});
+  )
+  next()
+})
 
-export const User = model<IUser, UserModel>("User", userSchema);
+export const User = model<IUser, UserModel>('User', userSchema)
