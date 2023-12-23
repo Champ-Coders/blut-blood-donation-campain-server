6a784b039e1ad53caa362dde2cce7faa29f82558
Nur Mohammad <mohammadnur5480@gmail.com>
2023-12-23 16:29:30 +0600

    feat: added review conterller

---
+++ src/app/modules/review/review.controller.ts
@@ -0,0 +1,69 @@
+import { Request, Response } from 'express'
+import httpStatus from 'http-status'
+import catchAsync from '../../../shared/catchAsync'
+import sendResponse from '../../../shared/sendResponse'
+import { ReviewService } from './review.service'
+import { IReview } from './review.interface'
+
+const insertIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const data = req.body
+  const result = await ReviewService.insertIntoDB(data)
+  sendResponse<IReview>(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Review Create Successfully!',
+    data: result,
+  })
+})
+
+const getAllData = catchAsync(async (req: Request, res: Response) => {
+  const result = await ReviewService.getAllData()
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'All Data Retrieved Successfully!',
+    data: result,
+  })
+})
+
+const getSingleData = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params
+  const result = await ReviewService.getSingleData(id)
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'All Data Retrieved Successfully!',
+    data: result,
+  })
+})
+
+const updateData = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params
+  const data = req.body
+  const result = await ReviewService.updateData(id, data)
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Data Updated Successfully!',
+    data: result,
+  })
+})
+
+const deleteData = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params
+  const result = await ReviewService.deleteData(id)
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Data Deleted Successfully!',
+    data: result,
+  })
+})
+
+export const ReviewController = {
+  insertIntoDB,
+  getAllData,
+  getSingleData,
+  updateData,
+  deleteData,
+}
