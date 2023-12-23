d33c60da73eb8a85615477dff847532464c3b47b
Nur Mohammad <mohammadnur5480@gmail.com>
2023-12-23 19:19:33 +0600

    feat: added volunteers routes

---
+++ src/app/modules/volunteers/volunteers.routes.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import { VolunteersController } from './volunteers.controller'
+
+const router = express.Router()
+
+router.get('/', VolunteersController.getAllData)
+router.get('/:id', VolunteersController.getSingleData)
+router.patch('/:id', VolunteersController.updateData)
+router.delete('/:id', VolunteersController.deleteData)
+router.post('/create-volunteer', VolunteersController.insertIntoDB)
+
+export const FaqsRoutes = router
