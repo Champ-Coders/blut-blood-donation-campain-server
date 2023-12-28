9e2bb09776b9c3d1dae8e7ba47adff438670d7cd
sarwar-asik <sarwarasik@gmail.com>
2023-12-28 16:11:20 +0600

    contact_almost_finished

--- src/app/modules/contact/sendMail.ts
+++ src/app/modules/contact/sendMail.ts
@@ -1,22 +1,23 @@
 import nodemailer from 'nodemailer'
 import config from '../../../config'
 
-export async function sendMailer(subject: string, to: string, html: string) {
+export async function sendMailer(subject: string, from: string, html: string) {
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com.', //!  gmail host and port for smtp ## Searchby= smtp host in browser
     port: 587, //!  port for smtp
     // secure: true,
     auth: {
-      user: config.nodeMailer.FromEmail, //! owner email
+      user: from, //! owner email
       pass: config.nodeMailer.appPassword,
     },
   })
 
-  await transporter.sendMail({
+  const sentResponse = await transporter.sendMail({
     from: config.nodeMailer.FromEmail,
-    to,
+    to: 'masudhossainmbs129@gmail.com',
     subject,
     // text: 'Hello world?',
     html,
   })
+  return sentResponse
 }
