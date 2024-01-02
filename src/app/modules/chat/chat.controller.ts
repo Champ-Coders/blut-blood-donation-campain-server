54209c4c2b24008499f006f9bc89ad531859c373
sarwar-asik <sarwarasik@gmail.com>
2024-01-02 17:48:05 +0600

    chat_service

--- src/app/modules/chat/chat.controller.ts
+++ src/app/modules/chat/chat.controller.ts
@@ -3,43 +3,43 @@ import catchAsync from '../../../shared/catchAsync'
 import sendResponse from '../../../shared/sendResponse'
 import { IChat } from './chat.interface'
 import httpStatus from 'http-status'
+import { chatService } from './chat.service'
 
 const createMessage = catchAsync(async (req: Request, res: Response) => {
   const data = req.body
   console.log('🚀 ~ file: chat.controller.ts:9 ~ createMessage ~ data:', data)
 
-  // const result = await EventService.insertIntoDB(data)
+  const result = await chatService.createmessage(data)
   sendResponse<IChat>(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'message Create Successfully!',
-    //   data: result,
+    data: result,
   })
 })
 
 const getAllMessagedUser = catchAsync(async (req: Request, res: Response) => {
-  //   const result = await EventService.getAllData()
+  const result = await chatService.getAllMessagedUser()
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'All Data Retrieved Successfully!',
-    // data: result,
+    message: 'All user Retrieved Successfully!',
+    data: result,
   })
 })
 
 const getSingleUserMessage = catchAsync(async (req: Request, res: Response) => {
-  const id = req.params.id
-  console.log(
-    '🚀 ~ file: chat.controller.ts:32 ~ getSingleUserMessage ~ id:',
-    id
-  )
+  const { senderEmail, receiverEmail } = req.body
 
-  //   const result = await EventService.getSingleData(id)
+  const result = await chatService.getSIngleUserMessage(
+    senderEmail,
+    receiverEmail
+  )
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: 'All Data Retrieved Successfully!',
-    // data: result,
+    message: 'All Message Retrieved Successfully!',
+    data: result,
   })
 })
 
